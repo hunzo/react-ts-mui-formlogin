bfd12889a57c0d0ab2582bb4faf1b723727ff0fe
Setsuna F. Seiei <takraw_s@hotmail.com>
2021-01-24 00:19:31 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import InfoPages from './InfoPages';
+import LoginPages from './LoginPages';
+import LoginImg from './LoginImg'
+import LoginStd from './LoginStd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={LoginPages} />
+                    <Route path="/info" component={InfoPages} />
+                    <Route path="/loginimg" component={LoginImg} />
+                    <Route path="/loginstd" component={LoginStd} />
+                    <Route exaxt path="/" render={() => (
+                        <p>start page</p>
+                    )} />
+                </Switch>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/InfoPages.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface props {
+    username?: string
+}
+
+const InfoPages: React.FC = () => {
+    return (
+        <div>
+            Info Pages
+        </div>
+    )
+}
+
+export default InfoPages
